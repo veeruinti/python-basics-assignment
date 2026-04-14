16c60d7e6cf268975efebf21b228c3b72ed2f018
Veeranjaneya inti <121534918+veeruinti@users.noreply.github.com>
2026-04-14 23:13:22 +0530

    Add basic Python tasks for age and personal info

---
+++ python_basics_veeranjaneyaInti.py
@@ -0,0 +1,22 @@
+# task 1:
+name = "Veeru"
+age = 21
+height = 1.75
+is_student = True
+
+print(name, age, height, is_student)
+
+# task 2:
+print(f"Name: {name} | Age: {age} | Height: {height} | Student: {is_student}")
+
+# task 3:
+# Age calculations
+age_in_months = age * 12
+age_in_days = age * 365
+remainder = age % 7
+age_square = age ** 2
+
+print("Age in months:", age_in_months)
+print("Age in days:", age_in_days)
+print("Remainder when age is divided by 7:", remainder)
+print("Age squared:", age_square)
